b63f03121801d50b4a9320118ce0b18f218a5a4f
Wesley Archbell <wesleyarchbell@gmail.com>
2017-06-29 08:54:44 +1000

    Add menu functionality

--- MainMenuScene.swift
+++ MainMenuScene.swift
@@ -14,5 +14,24 @@ class MainMenuScene: SKScene {
         
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let node = nodes(at: location)[0]
+            if node.name == "StartGameButton" {
+                showScene(scene: GameplayScene(fileNamed: "GameplayScene")!)
+            } else if node.name == "HighScoreButton" {
+                showScene(scene: HighScoreScene(fileNamed: "HighScoreScene")!)
+            } else if node.name == "OptionsButton" {
+                showScene(scene: OptionScene(fileNamed: "OptionScene")!)
+            }
+        }
+    }
+    
+    func showScene(scene: SKScene) {
+        scene.scaleMode = SKSceneScaleMode.aspectFill
+        self.view?.presentScene(scene, transition: SKTransition.doorsCloseHorizontal(withDuration: 1))
+    }
+    
 }
 
